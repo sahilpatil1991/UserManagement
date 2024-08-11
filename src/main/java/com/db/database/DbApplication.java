4898bfcdc3c873e1bf10638405ec7d1c91f9c8a4
sahilpatil1991 <83800196+sahilpatil1991@users.noreply.github.com>
2024-08-11 14:39:43 +0530

    Update DbApplication.java

--- src/main/java/com/db/database/DbApplication.java
+++ src/main/java/com/db/database/DbApplication.java
@@ -2,12 +2,18 @@ package com.db.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DbApplication {
+public class DbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DbApplication.class);
+    }
 }
